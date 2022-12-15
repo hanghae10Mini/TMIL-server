aa1f1b8376d9fa6f1593c20311a1474d12edde6e
Shinwon Jang <57736547+synuns@users.noreply.github.com>
2022-12-15 20:03:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,62 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+/* eslint-disable import/no-unresolved */
+const jsonServer = require('json-server');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-// Export the Server API
-module.exports = server
+// db.json를 조작하기 위해 lowdb를 사용
+let adapter;
+let db;
+
+const setDB = () => {
+  adapter = new FileSync('db.json');
+  db = low(adapter);
+};
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+
+server.post('/auth/comments/:id', (req, res) => {
+  setDB();
+  const { id } = req.params;
+  const { password } = req.body;
+  const data = db
+    .get('comments')
+    .find({
+      id: Number(id),
+      password,
+    })
+    .value();
+  if (data) {
+    res.send({ result: true, message: 'success' });
+  } else {
+    res.send({ result: false, message: 'failed' });
+  }
+});
+
+server.post('/auth/posts/:id', (req, res) => {
+  setDB();
+  const { id } = req.params;
+  const { password } = req.body;
+  const data = db
+    .get('posts')
+    .find({
+      id: Number(id),
+      password,
+    })
+    .value();
+  if (data) {
+    res.send({ result: true, message: 'success' });
+  } else {
+    res.send({ result: false, message: 'failed' });
+  }
+});
+
+server.use(router);
+
+server.listen(3001, () => {
+  console.log('JSON Server is running');
+});
